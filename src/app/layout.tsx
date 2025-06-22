803bb4dc80007368495b273968aa15d222aed477
jennymakki <jjennyeklundd@hotmail.com>
2025-06-22 11:06:55 +0200

    added a favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} antialiased`}
       >
