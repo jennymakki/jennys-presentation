9436b544f1bf2e1099fd58192cdf4c296f06bc47
jennymakki <jjennyeklundd@hotmail.com>
2025-08-30 16:12:57 +0200

    Added some new skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,7 +1,7 @@
 export const skills =
     {     
-        frontend: ["React", "Next.js", "Tailwind", "HTML", "CSS", "TypeScript", "EJS"],
+        frontend: ["React", "Next.js", "Tailwind", "HTML", "CSS", "TypeScript", "EJS", "JavaScript", "Java"],
         backend: ["MongoDB", "Express", "Node.js", "REST API"],
-        tools: ["Git", "Figma", "VSCode", "Docker", "Cypress", "Jest"],
+        tools: ["Git", "Figma", "VSCode", "IntelliJ", "Docker", "Cypress", "Jest"],
         softSkills: ["Agile", "Communication", "Problem-solving", "Collaboration", "Attention to Detail"]
     };
