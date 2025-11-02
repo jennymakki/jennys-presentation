2df6df4ed21a81d98f1f423d30f0ce42ea29ab2a
jennymakki <jjennyeklundd@hotmail.com>
2025-11-02 16:19:21 +0100

    added more skills I've learned.

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,7 +1,7 @@
 export const skills =
     {     
-        frontend: ["React", "Next.js", "Tailwind", "HTML", "CSS", "TypeScript", "EJS", "JavaScript"],
-        backend: ["MongoDB", "Express", "Node.js", "REST API", "CMS-adapter", "Java"],
-        tools: ["Git", "Figma", "VSCode", "IntelliJ", "Docker", "Cypress", "Jest"],
+        frontend: ["React", "Next.js", "Tailwind", "HTML", "CSS", "TypeScript", "EJS", "JavaScript", "Thymeleaf"],
+        backend: ["MongoDB", "Express", "Node.js", "REST API", "CMS-adapter", "Java", "Spring Boot", "Spring Data JPA", "Spring Security", "MySQL"],
+        tools: ["Git", "Figma", "VSCode", "IntelliJ", "Docker", "Docker Compose", "Cypress", "Jest"],
         softSkills: ["Agile", "Communication", "Problem-solving", "Collaboration", "Attention to Detail"]
     };
