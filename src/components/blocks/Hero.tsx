d1766fa4d8e577463cc0c0eba6a0d6b8af0d3cae
jennymakki <jjennyeklundd@hotmail.com>
2026-05-10 11:38:30 +0200

    formatting Hero

--- src/components/blocks/Hero.tsx
+++ src/components/blocks/Hero.tsx
@@ -102,8 +102,8 @@ export default function Hero() {
           </p>
 
           <p className="text-sm italic text-white/60">
-            For me, programming is a creative space where structure, collaboration,
-            and experimentation meet.
+            For me, programming is a creative space where structure,
+            collaboration, and experimentation meet.
           </p>
 
           <div className="flex gap-4 justify-center md:justify-start pt-2 pb-10">
